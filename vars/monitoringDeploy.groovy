216fb18b9f2393bf5923c896ded08fc1568eec95
elaine <yeelaine@uchicago.edu>
2026-03-11 16:41:12 -0500

    fix: add kube-context and increase timeout for helm install

--- vars/monitoringDeploy.groovy
+++ vars/monitoringDeploy.groovy
@@ -4,18 +4,23 @@ def call(Map config = [:]) {
     echo "MONITORING DEPLOY: Prometheus + Grafana in namespace=${namespace}"
 
     sh """
-        kubectl get namespace ${namespace} || kubectl create namespace ${namespace}
+        kubectl get namespace ${namespace} --context=ecommerce-dev || \
+            kubectl create namespace ${namespace} --context=ecommerce-dev
 
         helm repo add prometheus-community https://prometheus-community.github.io/helm-charts || true
         helm repo update
 
         helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
             --namespace ${namespace} \
+            --kube-context ecommerce-dev \
             --set grafana.adminPassword=admin123 \
             --set prometheus.prometheusSpec.scrapeInterval=15s \
-            --wait --timeout=120s
+            --set grafana.resources.requests.memory=128Mi \
+            --set grafana.resources.limits.memory=256Mi \
+            --timeout=600s \
+            --wait
 
         echo "Monitoring stack deployed"
-        kubectl get pods -n ${namespace}
+        kubectl get pods -n ${namespace} --context=ecommerce-dev
     """
 }
